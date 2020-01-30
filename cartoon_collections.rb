c494f540948a22a44cb31dc817adcaf04993575c
Norberto Santiago <norbertosantiago82@gmail.com>
2020-01-30 06:21:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,6 +2,7 @@ def roll_call_dwarves(array)
   index = 0 
   array.each_with_index do {|name, index|
     puts "#{index + 1} #{name}"}
+  end
 end
 
 def summon_captain_planet# code an argument here
