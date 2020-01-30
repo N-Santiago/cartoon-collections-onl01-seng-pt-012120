2cb2828dfd446a2a0c5d508458e663a039d1db2e
Norberto Santiago <norbertosantiago82@gmail.com>
2020-01-30 06:33:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,9 +5,8 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  planeteer_call []
   array.collect do |calls|
-    planeteer_call << "#{calls.capitalize}!"
+    array << "#{calls.capitalize}!"
   end 
   planeteer_call(array)
 end
